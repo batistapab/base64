c017e1e9102c4dba457f6963e3323e3ec521248a
Pablo Batista <pablobatista@outlook.com>
2022-04-03 10:48:03 -0300

    Update upload.php
    Ajustes no código

--- upload.php
+++ upload.php
@@ -3,13 +3,13 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["imagem"]["name"]);
 $uploadOk = 1;
 $extensaoDaImagem = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
+// Verifica se o arquivo atual é uma imagem ou não
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["imagem"]["tmp_name"]);
     if($check !== false) {
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "Este arquivo não é uma imagem!";
         $uploadOk = 0;
     }
 }
@@ -19,18 +19,20 @@ if ($_FILES["imagem"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Permitir formatos expecificos de imagens
-if($extensaoDaImagem != "jpg" && $extensaoDaImagem != "png" && $extensaoDaImagem != "jpeg"
-&& $extensaoDaImagem != "gif" ) {
+if($extensaoDaImagem != "jpg" && $extensaoDaImagem != "png" && $extensaoDaImagem != "jpeg" && $extensaoDaImagem != "gif" ) {
     echo "Desculpe, somente arquivos do tipo JPG, JPEG, PNG & GIF são permitidos.";
     $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error
+// Verifica $uploadOk se tiver erro recebe o valor 0 e exibe o erro
 if ($uploadOk == 0) {
     echo "Desculpe, sua imagem não pode ser enviada.";
-// if everything is ok, try to upload file
+// Se estiver tudo ok exibe o resultado abaixo ou você pode implementar a inserção do texto da conversão usando base64 para o banco 
 } else {
-		echo '<img src="data:image/png;base64,'. base64_encode(file_get_contents($_FILES["imagem"]["tmp_name"])) .'" alt="" />';
+	
+		$imagem = base64_encode(file_get_contents($_FILES["imagem"]["tmp_name"]));
+	
+		echo sprintf('<img src="data:image/png;base64,%s" alt="" />',$imagem);
 		//Caso você prefira salvar a imagem como base 64.
-       echo "<br /><h1>Imagem convertida em base64 </h1><textarea cols='120' rows='7'>".base64_encode(file_get_contents($_FILES["imagem"]["tmp_name"]))."</textarea>";
+       		echo sprintf('<br /><h1>Imagem convertida em base64 </h1><textarea cols='120' rows='7'>%s</textarea>',$imagem);;
 }
 ?>
